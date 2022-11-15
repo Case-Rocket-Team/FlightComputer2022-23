c8970a8c5dd6df8e112a61b9bc6d34df65ede2e7
Serena <sjtl2004@gmail.com>
2022-11-15 00:34:54 -0500

    added header for log

--- src/log.cpp
+++ src/log.cpp
@@ -1,10 +1,5 @@
+#include "log.hpp"
 #include "Arduino.h"
-
-#define LOG_NULL_PACKET_ID          0x00
-#define LOG_ANNOTATION_PACKET_ID    0x01
-#define LOG_I32_PACKET_ID           0x02
-#define LOG_F32_PACKET_ID           0x03
-#define LOG_F64_PACKET_ID           0x04
 
 void write(uint8_t byte) {
     // TODO
---
+++ src/log.hpp
@@ -0,0 +1,18 @@
+#ifndef LOG_
+#define LOG_
+
+#define LOG_NULL_PACKET_ID          0x00
+#define LOG_ANNOTATION_PACKET_ID    0x01
+#define LOG_I32_PACKET_ID           0x02
+#define LOG_F32_PACKET_ID           0x03
+#define LOG_F64_PACKET_ID           0x04
+
+#include <Arduino.h>
+
+template <typename T>
+void log(T data);
+
+template <typename T>
+void log(T data, String* annotation);
+
+#endif
