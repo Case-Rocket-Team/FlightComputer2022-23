76ab19126265170af361f23cf3c492cb157ec6e5
Serena <sjtl2004@gmail.com>
2022-12-01 18:53:46 -0500

    Spacing

--- src/subsystems/pyro_subsystem.cpp
+++ src/subsystems/pyro_subsystem.cpp
@@ -6,6 +6,7 @@
 bool PyroSubsystem::success() {
     // TODO
 }
+
 /// @brief makes the pyro take action based on the derived state passed
 /// @param state the derived state to act on
 void PyroSubsystem::apply_state(DerivedState &state) {
