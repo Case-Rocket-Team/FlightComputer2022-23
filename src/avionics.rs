a0643a8805f2ef4c7dd4cea2bb30c6f92e918e27
Serena <sjtl2004@gmail.com>
2023-04-03 10:45:35 -0400

    Refactored avionics, spi

--- src/avionics.rs
+++ src/avionics.rs
@@ -1,12 +1,14 @@
-use bsp::board::{Resources, self};
+use bsp::board;
 use imxrt_hal::{self, timer::Blocking, pit::Pit};
 use teensy4_bsp as bsp;
 
-use crate::{spi::{spi_manager::{SPIManager, SPIPins}}};
+use crate::spi::spi_manager::{SPIManager, SPIPins};
+
+pub type Timer = Blocking<Pit<0>, { board::PERCLK_FREQUENCY }>;
 
 pub struct Avionics {
     pub spi: SPIManager,
-    pub timer: Blocking<Pit<0>, { board::PERCLK_FREQUENCY }>
+    pub timer: Timer,
 }
 
 pub fn get_avionics() -> Avionics {
@@ -16,14 +18,11 @@ pub fn get_avionics() -> Avionics {
         pit: (pit, _, _, _),
         usb,
         mut gpio1,
-        gpio2,
-        gpio3,
-        gpio4,
         ..
     } = board::t40(board::instances());
 
     bsp::LoggingFrontend::default_log().register_usb(usb);
-    let mut timer = Blocking::<_, { board::PERCLK_FREQUENCY }>::from_pit(pit);
+    let timer = Timer::from_pit(pit);
     
     let spi_manager = SPIManager::new(lpspi4, SPIPins {
         flash: gpio1.output(pins.p1)
